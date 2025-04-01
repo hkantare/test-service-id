c981035aff89a1c6451516c70a2f472b9218a8f5
HARINI KANTAREDDY <hkantare@in.ibm.com>
2025-04-01 22:41:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,10 @@ variable "ibmcloud_api_key" {
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+    }
+  }
+}
