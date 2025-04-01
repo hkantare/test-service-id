f84943a970106908e3648ff7081fd772f8b38b57
HARINI KANTAREDDY <hkantare@in.ibm.com>
2025-04-01 22:36:54 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+# This data block gets the account configuration where service ids and authorisation policy is provisioned
+data "ibm_iam_account_settings" "sid_auth_policy_account_settings" {
+}
+
+
+variable "ibmcloud_api_key" {
+  type        = string
+  description = "The IBM Cloud API Key where Service ID API Key will be stored in secrets manager"
+  sensitive   = true
+}
+
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+}
